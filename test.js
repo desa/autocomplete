c4c4d452786d8c0f0062b6cfcace40d4effa6a59
Michael Desa <mjdesa@gmail.com>
2015-05-20 11:35:41 -0700

    update example code

--- test.js
+++ test.js
@@ -11,5 +11,6 @@ ex.addWord("leap");
 console.log("he",ex.otherLookup("he"));
 console.log("hel",ex.otherLookup("hel"));
 console.log("hea",ex.otherLookup("hea"));
+console.log("hed",ex.otherLookup("hed"));
 console.log("l",ex.otherLookup("l"));
 console.log("d", ex.otherLookup("d"));
